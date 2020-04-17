0a8f8065eb7b9e5a1063916cbd52a0081c804e36
Mousam Dhakal <mousamdhakal7.md@gmail.com>
2020-04-17 10:47:33 +0545

    Added styling

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,72 +1,79 @@
 let timer = 2;
 let isBreak = true;
-let pause = true;
+let isPaused = true;
+
+let viewTimer = document.getElementById('timer');
+
+let pauseHandler = document.getElementById('pause');
+pauseHandler.addEventListener('click', pause);
+
+let playHandller = document.getElementById('play');
+playHandller.addEventListener('click', play);
+
+let resetHandler = document.getElementById('reset');
+resetHandler.addEventListener('click', reset);
+
+let stopHandler = document.getElementById('stop');
+stopHandler.addEventListener('click', stop);
 
 let sessionTimeHandler = document.getElementById('session');
+sessionTimeHandler.addEventListener('change', setTime);
 
 let breakTimeHandler = document.getElementById('break');
+breakTimeHandler.addEventListener('change', setTime);
 
 setInterval(update, 1000);
 
-function update(){
-    if(!pause) timer--;
-    if(timer === 0) switchTimer();
-    viewTimer.innerText = (displayTimer(timer));
+function update() {
+	if (!isPaused) timer--;
+	if (timer === 0) switchTimer();
+	viewTimer.innerText = displayTimer(timer);
 }
 
-function reset(){
-    breakTime = 5;
-    sessionTime = 25;
-    stop();
+function pause() {
+	isPaused = true;
 }
 
-function stop(){
-    timer = (isBreak)? +breakTimeHandler.value * 60:
-                       +sessionTimeHandler.value * 60;
-    pause = true;
-    update();
+function reset() {
+	breakTimeHandler.value = '5';
+	sessionTimeHandler.value = '25';
+	stop();
 }
 
-function switchTimer(){
-    if(!isBreak) timer = +breakTimeHandler.value * 60;
-    else timer = +sessionTimeHandler.value * 60;
-    alertSwitch();
-    isBreak = !isBreak;
+function play() {
+	isPaused = false;
+	breakTimeHandler.disabled = sessionTimeHandler.disabled = false;
 }
 
-function displayTimer(timer){
-    let minutes = Math.floor(timer/60),
-        seconds = timer % 60;
-    let extraZero = (seconds < 10)? '0':'';
-    return `${minutes}:${extraZero}${seconds}`;
+function setTime() {
+	timer = isBreak ? +breakTimeHandler.value * 60 : +sessionTimeHandler.value * 60;
 }
 
-function alertSwitch(){
-    if(isBreak) alert('Back to work!');
-    if(!isBreak) alert('Break time!');
+function stop() {
+	isPaused = true;
+	setTime();
+	breakTimeHandler.disabled = sessionTimeHandler.disabled = false;
+	update();
 }
-let content = document.getElementById('container');
-let viewTimer = document.createElement('p');
-content.appendChild(viewTimer);
 
+function switchTimer() {
+	if (!isBreak) timer = +breakTimeHandler.value * 60;
+	else timer = +sessionTimeHandler.value * 60;
+	alertSwitch();
+	isBreak = !isBreak;
+}
 
-pauseHandler = document.getElementById('pause');
-pauseHandler.addEventListener('click', function(){
-    pause = true;
-})
+function displayTimer(timer) {
+	let minutes = Math.floor(timer / 60),
+		seconds = timer % 60;
+	let extraZero = seconds < 10 ? '0' : '';
+	return `${minutes}:${extraZero}${seconds}`;
+}
 
-playHandller = document.getElementById('play');
-playHandller.addEventListener('click', function(){
-  pause= false;  
-})
-
-resetHandler = document.getElementById('reset');
-resetHandler.addEventListener('click', reset);
-
-stopHandler = document.getElementById('stop');
-stopHandler.addEventListener('click', stop);
-
-
+function alertSwitch() {
+	if (isBreak) alert('Back to work!');
+	if (!isBreak) alert('Break time!');
+}
 
 // function getInputValue(){
 //     let sessionValue = document.getElementById("session").value;
